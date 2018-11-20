46bbd147c58f7cbf1047cac7988bd3adcf3b872a
Maria Nosareva <mashunya_19-97@mail.ru>
2018-11-20 18:11:47 +0300

    fibonacci numbers + getting first n types in typelist + hierarchies

--- TypeList/main.cpp
+++ TypeList/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <typeinfo>
 
 /* --------------------------------------------------- TYPELIST -----------------------------------------------------*/
 
@@ -15,6 +16,19 @@ struct TypeList<T> {
   using head = T;
   using tail = NullClass;
 };
+
+/* --------------------------------------------------- PRINT --------------------------------------------------------*/
+
+template <typename TL>
+void printTL() {
+  std::cout << typeid(typename TL::head).name() << " ";
+  printTL<typename TL::tail>();
+};
+
+template <>
+void printTL<NullClass>() {
+  return;
+}
 
 /* --------------------------------------------------- LENGTH -------------------------------------------------------*/
 
@@ -76,7 +90,7 @@ struct RemoveType<TypeList<Head, Tail ... >, index> {
   using result = typename AddType<typename RemoveType<TypeList<Tail ...>, index - 1>::result, 0, Head>::result;
 };
 
-/* --------------------------------------------------- CHANGE -------------------------------------------------------*/
+/* -------------------------------------------------- CHANGE --------------------------------------------------------*/
 
 template <typename T, unsigned int index, typename E> struct ChangeType;
 
@@ -90,10 +104,89 @@ struct ChangeType<TypeList<Head, Tail ...>, index, Elem> {
   using result = typename AddType<typename ChangeType<TypeList<Tail ...>, index - 1, Elem>::result, 0, Head>::result;
 };
 
+/* ------------------------------------------- GET FIRST N ELEMENTS  ------------------------------------------------*/
 
-typedef TypeList<int, double, std::string> TL;
+template <typename TL, unsigned int HowMany> struct CutFirstN;
+
+template <typename TL, unsigned int HowMany>
+struct CutFirstN {
+  using result = typename AddType<typename CutFirstN<TL, HowMany - 1>::result,
+                                  HowMany - 1,
+                                  typename TypeAt<TL, HowMany - 1>::result>::result;
+};
+
+template <typename TL>
+struct CutFirstN<TL, 0> {
+  using result = TypeList<>;
+};
+
+/* --------------------------------------- GENERATING SCATTER HIERARHY ----------------------------------------------*/
+
+template <class T>
+struct Unit {
+  T value;
+};
+
+template <typename TL, template <typename> typename U>  struct GSH;
+
+template <typename Head, template <typename> typename U, typename ... Tail>
+struct GSH<TypeList<Head, Tail ...>, U>: public U<Head>,
+            public GSH<TypeList<Tail ...>, U> {
+  using TL = TypeList<Head, Tail ...>;
+  using RightTree = U<Head>;
+  using LeftTree = GSH<TypeList<Tail ...>, U>;
+};
+
+template <template <typename> typename U>
+struct GSH<TypeList<>, U> {  };
+
+/* --------------------------------------- GENERATING LINEAR HIERARHY -----------------------------------------------*/
+
+template <class T, class Base>
+struct LinearUnit: Base {
+  T value;
+};
+
+template <typename TL, template <class, class> typename U, class Root = NullClass>  struct GLH;
+
+template <typename Head, template <class, class> typename U, class Root, typename ... Tail>
+struct GLH<TypeList<Head, Tail ...>, U, Root>: public U<Head, GLH<TypeList<Tail ...>, U, Root>> {};
+
+template <typename Tail, template <class, class> typename U, class Root>
+struct GLH<TypeList<Tail>, U, Root>: public LinearUnit<Tail, Root> {};
+
+/* --------------------------------------- GENERATING FIBONACCI NUMBERS ---------------------------------------------*/
+
+template <int T>
+struct Fibonacci {
+  enum { value = Fibonacci<T - 1>::value + Fibonacci<T - 2>::value };
+};
+
+template <>
+struct Fibonacci<1> {
+  enum { value = 1 };
+};
+
+template <>
+struct Fibonacci<2> {
+  enum { value = 1 };
+};
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+class First {
+ public:
+  std::string fisrtString = "hello";
+  std::string getFirstString() const {
+    return fisrtString;
+  }
+};
+
+typedef TypeList<First, std::string, First> TL;
+typedef GSH<TL, Unit> SH;
+typedef GLH<TL, LinearUnit> LH;
+// typedef CutFirstN<TL, 2>::result CuttedTL;
 
 int main() {
-  TypeAt<ChangeType<TL, 1, int>::result, 1>::result a = 3.5;
   return 0;
 }
